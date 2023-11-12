6191e330e168b0d0b0b8157e25b5585024259ff7
dias901 <lucass-dias@hotmail.com>
2023-11-12 15:56:16 -0300

    add exceções

--- src/Boardgame/Board.java
+++ src/Boardgame/Board.java
@@ -1,5 +1,7 @@
 package Boardgame;
 
+import Exception.BoardException;
+
 public class Board {
 
 	private int linha;
@@ -7,6 +9,9 @@ public class Board {
 	private Peça[][] peças;
 	
 	public Board(int linha, int coluna) {
+		if(linha<1 || coluna <1) {
+			throw new BoardException("Tabuleiro não pode ser criado. Valor minimo: 1");
+		}
 		this.linha = linha;
 		this.coluna = coluna;
 		peças = new Peça[linha][coluna];
@@ -16,28 +21,33 @@ public class Board {
 		return linha;
 	}
 
-	public void setLinha(int linha) {
-		this.linha = linha;
-	}
-
 	public int getColuna() {
 		return coluna;
 	}
-
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
-	}
 	
 	public Peça peça(int linha, int coluna) {
+		if(!PosiçaoExist(linha,coluna)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
 		return peças[linha][coluna];
 	}
 	
-	public Peça peça(Posição posiçao) {
-		return peças[posiçao.getLinha()][posiçao.getColuna()];
-	}
-	
 	public void placePeça(Peça peça, Posição pos) {
+		if(therePeça(pos.getLinha(),pos.getColuna())) {
+			throw new BoardException("Já existe uma peça na posição: "+pos.getLinha()+","+pos.getColuna());
+		}
 		peças[pos.getLinha()][pos.getColuna()] = peça;
 		peça.pos = pos;
+	}
+	
+	public boolean PosiçaoExist(int linhas, int colunas) {
+		return linhas >=0 && linhas<linha && colunas >=0 && colunas<coluna;
+	}
+	
+	public boolean therePeça(int linha, int coluna) {
+		if(!PosiçaoExist(linha, coluna)) {
+			throw new BoardException("Posição fora do tabuleiro");
+		}
+		return peça(linha,coluna) != null;
 	}
 }
